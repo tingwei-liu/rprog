a489eebb5dff2c0970a8ad35c4bbf1e2f19a5d80
PF <PF>
2015-11-10 03:56:56 +0800

    a little modification of the addition method & added multiplication!

--- prismsus/PFPF/high-accuracy_algorithms/High-accuracy multiplication.R
+++ prismsus/PFPF/high-accuracy_algorithms/High-accuracy multiplication.R
@@ -52,16 +52,48 @@ high_accuracy_add <- function(A, B)
 	return(Result)
 }
 
+# adds zeros to the string
+add_zeros <- function(A, n)
+{
+	for (i in 1:n) {
+		A = paste0(A, "0")
+	}
+	return(A)
+}
+
+high_accuracy_mult <- function(A, B)
+{
+	# nchar returns the length of the string.
+	lA <- nchar(A)
+	lB <- nchar(B)
+
+	# if it's simple to calculate, we do it directly.
+	if (lA + lB < 10) return(as.character(as.integer(as.integer(A) * as.integer(B))))
+	
+	# to swap two strings, we create another string called C to store the temporary data.
+	if (lA < lB) { 
+		C <- A
+		A <- B
+		B <- C
+		lC <- lA
+		lA <- lB
+		lB <- lC
+	}
+	
+	# now that A is longer than B, we cut A into halves!
+	return(high_accuracy_add(add_zeros(high_accuracy_mult(substr(A, 1, lA / 2), B), (lA + 1) / 2), high_accuracy_mult(substr(A, lA / 2 + 1, lA), B)))
+}
+
 
 
 
 
 # introduction on what does this program do.
-print("This program allows you to add any positive integer to another.")
+print("This program allows you to multiply any positive integer to another.")
 
 # you may need to run these line by line.
 A <- readS()
 B <- readS()
 
 # print.
-print(high_accuracy_add(A, B))
+print(high_accuracy_mult(A, B))
